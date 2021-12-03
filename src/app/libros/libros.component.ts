91aea03faa2ecaa26cf16e07c27934e2ffdd8592
Desarrollo 4 <ctcdesarrollo4@hotmail.com>
2021-12-02 23:42:15 -0500

    New Commits Nuevo

---
+++ src/app/libros/libros.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-libros',
+  templateUrl: './libros.component.html',
+})
+export class LibrosComponent {
+  libros = ['Matematica', 'Algoritmos Basicos', 'Algebra Nivel Basico'];
+}
